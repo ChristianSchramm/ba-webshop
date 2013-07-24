44771033a79a82a2781795fa16ff62c1bf3f5b98
Tobias Dietrich (Selen) <info@etooby.com>
2013-07-24 11:37:47 +0200

    [Task] add ide

--- src/php/src/src/Web/Bundle/ShopBundle/Controller/ProductController.php
+++ src/php/src/src/Web/Bundle/ShopBundle/Controller/ProductController.php
@@ -6,14 +6,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
-class DefaultController extends Controller
+class ProductController extends Controller
 {
-    /**
-     * @Route("/")
-     * @Template()
-     */
-    public function indexAction()
-    {
-        return array();
-    }
 }
