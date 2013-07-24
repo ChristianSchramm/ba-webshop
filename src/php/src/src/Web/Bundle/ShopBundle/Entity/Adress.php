3dc2716944cf03d9ed8ab9e98a5a20386a846b27
Tobias Dietrich <info@etooby.com>
2013-07-24 09:50:45 +0200

    [TASK] init symfony web bundle

---
+++ src/php/src/src/Web/Bundle/ShopBundle/Entity/Adress.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace Web\Bundle\ShopBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Adress
+ *
+ * @ORM\Table(name="adress")
+ * @ORM\Entity(repositoryClass="Web\Bundle\ShopBundle\Entity\AdressRepository")
+ */
+class Adress
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="Street", type="string", length=255)
+     */
+    private $street;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="Location", type="string", length=255)
+     */
+    private $location;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="Postcode", type="string", length=255)
+     */
+    private $postcode;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="Country", type="string", length=255)
+     */
+    private $country;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set street
+     *
+     * @param string $street
+     * @return Adress
+     */
+    public function setStreet($street)
+    {
+        $this->street = $street;
+    
+        return $this;
+    }
+
+    /**
+     * Get street
+     *
+     * @return string 
+     */
+    public function getStreet()
+    {
+        return $this->street;
+    }
+
+    /**
+     * Set location
+     *
+     * @param string $location
+     * @return Adress
+     */
+    public function setLocation($location)
+    {
+        $this->location = $location;
+    
+        return $this;
+    }
+
+    /**
+     * Get location
+     *
+     * @return string 
+     */
+    public function getLocation()
+    {
+        return $this->location;
+    }
+
+    /**
+     * Set postcode
+     *
+     * @param string $postcode
+     * @return Adress
+     */
+    public function setPostcode($postcode)
+    {
+        $this->postcode = $postcode;
+    
+        return $this;
+    }
+
+    /**
+     * Get postcode
+     *
+     * @return string 
+     */
+    public function getPostcode()
+    {
+        return $this->postcode;
+    }
+
+    /**
+     * Set country
+     *
+     * @param string $country
+     * @return Adress
+     */
+    public function setCountry($country)
+    {
+        $this->country = $country;
+    
+        return $this;
+    }
+
+    /**
+     * Get country
+     *
+     * @return string 
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+}
---
+++ src/php/src/src/Web/Bundle/ShopBundle/WebShopBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Web\Bundle\ShopBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class WebShopBundle extends Bundle
+{
+}
