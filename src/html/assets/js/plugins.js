dafd14fa4822e35a7db6764c3b2aec228869f01c
Christian Schramm <christian-schramm83@gmx.de>
2013-07-31 10:49:56 +0200

    [Task] Html-Layout

--- src/html/assets/js/plugins.js
+++ src/html/assets/js/plugins.js
@@ -22,3 +22,34 @@
 }());
 
 // Place any jQuery/helper plugins in here.
+
+/**********************************************************
+  @author mail@markus-falk.com
+  @description sets css min-height of given elements to the highest calculated value
+***********************************************************/
+
+;(function($) {
+
+  $.fn.mf_EqualHeight = function() {
+
+    var heights = 0;
+
+    this.each(function() {
+
+      // cache object height
+      var obj_height = $(this).height();
+
+      // save it if it is higher than before
+      if(heights < obj_height) {
+        heights = obj_height;
+      }
+    });
+
+    // set height of given elements to calculated value
+    this.css('min-height', heights);
+
+    // preserve chainability
+    return this;
+  };
+
+})(jQuery);
