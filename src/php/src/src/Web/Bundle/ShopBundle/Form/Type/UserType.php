01c931d6991c703fb1ea530044758f0f8bfcac4c
Tobias Dietrich (Selen) <info@etooby.com>
2013-09-23 18:24:45 +0200

    [Task] zeug so

--- src/php/src/src/Web/Bundle/ShopBundle/Form/Type/UserType.php
+++ src/php/src/src/Web/Bundle/ShopBundle/Form/Type/UserType.php
@@ -10,12 +10,28 @@ class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('email', 'email');
-        $builder->add('username', 'text');
+        $builder->add('email', 'email', array( 
+            'label'  => 'Email',
+            'attr'   =>  array( 'class'   => 'input'),
+				    'label_attr' =>  array( 'class'   => 'label'),
+            )
+        );
+        $builder->add('username', 'text', array( 
+            'label'  => 'Benutzername',
+            'attr'   =>  array( 'class'   => 'input'),
+				    'label_attr' =>  array( 'class'   => 'label'),
+            )
+        );
         $builder->add('password', 'repeated', array(
            'first_name' => 'password',
            'second_name' => 'confirm',
            'type' => 'password',
+        	 'first_options' => array(
+            'attr'   =>  array( 'class'   => 'input'),
+				    'label_attr' =>  array( 'class'   => 'label')),
+        	 'second_options' => array(
+            'attr'   =>  array( 'class'   => 'input'),
+				    'label_attr' =>  array( 'class'   => 'label')),
         ));
     }
 
